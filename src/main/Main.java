9ddf548741cb466f531c90dcc19184433fca85d3
Steven_Perez <Luis-steven25@hotmail.com>
2020-06-03 15:57:38 -0500

    Cambios en la Clase Principal

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
-        System.out.println(" Vamos TITO!!");
+        System.out.println("Bienvenido a la clase");
         System.out.println("");
     }
 }
