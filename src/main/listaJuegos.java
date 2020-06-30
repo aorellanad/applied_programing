70dcf6e83cbcc9bb6ef06a95feba380689820a43
Josue Montesdeoca <jmontesdeocas1@est.ups.edu.ec>
2020-06-30 16:11:51 -0500

    Se añadio un JFrame registroDeJuegos, JFrame listarDatosJuegos en el packete views y 2 clases en el paquete main, Datos y listaJuegos

---
+++ src/main/listaJuegos.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class listaJuegos {
+    ArrayList<Datos> lista = new ArrayList<Datos>();
+
+    public ArrayList<Datos> getLista() {
+        return lista;
+    }
+
+    public void Agregar(Datos p) {
+        lista.add(p);
+    }
+
+    public Datos BuscarxFecha(String nombre, String fechalan) {
+        Datos p = new Datos();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getNombre().equals(nombre)) {
+                if (lista.get(i).getFechaLan().equals(fechalan)) {
+                    return lista.get(i);
+                }
+
+            }
+        }
+        return p;
+    }
+
+    public void ModificarxFecha(Datos p) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getNombre().equals(p.getNombre())) {
+                if (lista.get(i).getFechaLan().equals(p.getFechaLan())) {
+                    lista.get(i).setPrecio(p.getPrecio());
+                    lista.get(i).setApto(p.getApto());
+                    lista.get(i).setConsola(p.getConsola());
+                    lista.get(i).setGenero(p.getGenero());
+                }
+            }
+        }
+    }
+
+    public void EliminarxFecha(String nombre, String fechalan) {
+        for (int i = 0; i < lista.size(); i++) {
+             if (lista.get(i).getNombre().equals(nombre)) {
+                if (lista.get(i).getFechaLan().equals(fechalan)) {
+                    lista.remove(i);
+                }
+            }
+        }
+    }
+}
