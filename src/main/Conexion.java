923d05ca8fb4e0ed1987925d1ff0ad7b31ec4aca
KeykoGarces <keyko_garces@hotmail.com>
2020-08-14 19:46:22 -0500

    Se añadió el botón agregar y modificar en la clase Tutoria_Ventana_Producto_DB

--- src/main/Conexion.java
+++ src/main/Conexion.java
@@ -1,60 +1,57 @@
 package main;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.*;
+import java.util.*;
 
 public class Conexion {
 
     private String servername = "localhost";
-    private String database = "Taller";
-    private String url = "jdbc:mysql://localhost:3307/" + database;
+    private String database = "taller 5";
+    private String url = "jdbc:mysql://localhost:3306/" + database;
     private String username = "root";
     private String password = "123";
-    
-    private Connection con=null;
-    private Statement stm=null;
-    
-    public Connection getCon(){
+    private Connection con = null;
+    private Statement stm = null;
+
+    public Connection getCon() {
         return con;
     }
-    public void setCon(Connection con){
+
+    public void setCon(Connection con) {
         this.con = con;
     }
-    public Statement getStm(){
+
+    public Statement getStm() {
         return stm;
     }
-    public void setStm(Statement stm){
-        this.stm=stm;
+
+    public void setStm(Statement stm) {
+        this.stm = stm;
     }
 
-    public Statement Conectar() {
+    public void Conectar() {
+
         try {
             Class.forName("org.gjt.mm.mysql.Driver");
             con = DriverManager.getConnection(url, username, password);
             stm = con.createStatement();
-            System.out.println("Coneccion a la base...");
+            System.out.println("Conectado a la base de datos");
         } catch (Exception ex) {
-            String ErrString = "error al conectar a la base de datos" + ex.getMessage();
+            String ErrString = "Error al conectar a la base de datos" + ex.getMessage();
             System.out.println(ErrString);
+
         }
-        return stm;
     }
-    
-    public void Desconectar()
-    {
-        if (this.con != null) {
-            try {
+    public void Desconectar(){
+        if(this.con!=null){
+            try{
                 this.stm.close();
                 this.con.close();
-            } catch (SQLException ex)
-            {
-                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null,ex);
-                //System.out.println("Error al desconectar");
+            }catch(SQLException ex){
+                System.out.println("Error al desconectar");
             }
         }
     }
 }
+
+
